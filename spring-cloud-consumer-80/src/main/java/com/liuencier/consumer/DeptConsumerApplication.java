7311e5113d46206308d1ccab258e7e70df4d11e9
liuenci <liuenci@cvte.com>
2020-05-03 22:57:39 +0800

    Spring cloud 自定义负载均衡策略

--- spring-cloud-consumer-80/src/main/java/com/liuencier/consumer/DeptConsumerApplication.java
+++ spring-cloud-consumer-80/src/main/java/com/liuencier/consumer/DeptConsumerApplication.java
@@ -1,8 +1,10 @@
 package com.liuencier.consumer;
 
+import com.liuencier.consumer.rule.CierRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @program: SpringCloudLearningCi
@@ -10,8 +12,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @author: liuenci
  * @create: 2020-05-02 16:24
  **/
+// Ribbon 和 Eureka 整合之后，客户端可以直接调用，不用关心 IP 地址和端口号
 @EnableEurekaClient
 @SpringBootApplication
+// 在微服务启动的时候就能去加载我们自定义的的Ribbon类
+@RibbonClient(name = "spring-cloud-provider-dept", configuration = CierRule.class)
 public class DeptConsumerApplication {
 
     public static void main(String[] args) {
